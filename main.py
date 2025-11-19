02a96f958276ad0cc410e9711b16e4cf2724235c
BORDA_Elorien <elorien.borda@gmail.com>
2025-11-19 17:51:43 +0100

    main et reseau

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 from reseau import Reseau
+import numpy as np
+
+def poidsInitiaux():
+    #renvoie une liste de poids générés aléatoirement
+    pass
+
+def importData():
+    #renvoie deux arrays : un pour les données d'entrainement et l'autre pour les données de test
+    pass
+
+#création du réseau
+poids = poidsInitiaux()
+reseau = Reseau(poids)
+
+#phase d'entrainement
+data_entrainement, data_test = importData()
+nb_iterations = 10
+reseau.descenteGradient(data_entrainement, nb_iterations)
+
+#phase de test
--- reseau.py
+++ reseau.py
@@ -1,7 +1,30 @@
 import numpy as np
 
 class Reseau:
-    """Réseau de neurones à une couche cachée"""
 
     def __init__(self, poids):
+        self.poids = poids #np array, la i-ième liste contient les poids associés au i-ième neurone de la couche cachée
         pass
+
+    def forwardProp(self, data):
+        #renvoie résultat du réseau quand on met data en input (nombre compris entre 0 et 1)
+        pass
+
+    def descenteGradient(self, data_entrainement, nb_iterations):
+        #change les poids avec l'algo du gradient
+        pass
+
+    def backProp(self, data, resultat_attendu):
+        #renvoie le gradient de la fonction de cout (array 1d)
+        pass
+
+    def fonctionCout(self, resultat, resultat_attendu):
+        pass
+
+
+
+def fonctionActivation(x):
+    pass
+
+def deriveeActivation(x):
+    pass
