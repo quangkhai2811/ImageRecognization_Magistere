2f35b4f340ae40f7a1e7fbea9201ea9f4b3cdc99
BORDA_Elorien <elorien.borda@gmail.com>
2025-11-20 12:07:06 +0100

    forwardprop

--- reseau.py
+++ reseau.py
@@ -2,29 +2,30 @@ import numpy as np
 
 class Reseau:
 
-    def __init__(self, poids):
-        self.poids = poids #np array, la i-ième liste contient les poids associés au i-ième neurone de la couche cachée
-        pass
+    def __init__(self, tailles_couches, fonctionActivation, poids_initiaux):
+        
+        self.tailles_couches = tailles_couches #liste avec la taille de chaque couche
+        self.nb_couches = len(self.tailles_couches)
+        self.fonctionActivation = fonctionActivation
+        self.poids = poids_initiaux #liste de matrices np, dont la taille est nb_couches-1
+        self.activations = [np.ones(taille+1) for taille in self.tailles_couches]
+
 
     def forwardProp(self, data):
-        #renvoie résultat du réseau quand on met data en input (nombre compris entre 0 et 1)
-        pass
+        self.activations[0][:-1] = data
+        for i in range(self.nb_couches-1):
+            self.activations[i+1][:-1] = self.fonctionActivation(np.dot(self.poids[i+1], self.activations[i]))
+
 
     def descenteGradient(self, data_entrainement, nb_iterations):
         #change les poids avec l'algo du gradient
         pass
 
+
     def backProp(self, data, resultat_attendu):
         #renvoie le gradient de la fonction de cout (array 1d)
         pass
 
+
     def fonctionCout(self, resultat, resultat_attendu):
         pass
-
-
-
-def fonctionActivation(x):
-    pass
-
-def deriveeActivation(x):
-    pass
